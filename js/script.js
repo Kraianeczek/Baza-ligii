d320d4040d85110c2a8982218b62aa952ad6ef08
Tomasz Krajewski <krajaneczek@gmail.com>
2021-12-27 14:25:05 +0100

    add active links

--- js/script.js
+++ js/script.js
@@ -8,4 +8,23 @@ for (let link of links) {
 
 function titleClickHandler() {
   console.log('Title was clicked');
+  const clickedElement = this;
+  const activeLinks = document.querySelectorAll('.titles a.active');
+    
+  for (let activeLink of activeLinks) {           /* remove class 'active' from all article links */
+    activeLink.classList.remove('active');
+  }
+
+  console.log('Clicked element:', clickedElement);
+  clickedElement.classList.add('active');           /* add class 'active' to the clicked link */
+                   
+  const activeArticles = document.querySelectorAll('.posts .active');
+  
+  for (let activeArticle of activeArticles) {      /* remove class 'active' from all articles */
+    activeArticle.classList.remove('active');
+  }
+                    /* get 'href' attribute from the clicked link */
+                    /* find the correct article using the selector (value of 'href' attribute) 
+                    /* add class 'active' to the correct article */
+
 }
